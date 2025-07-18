146598d1057127fbb656d5195915e1c582ea6298
aksharaparkala@gmail.com <aksharaparkala@gmail.com>
2025-07-18 15:07:54 +0530

    Added Addition.java program

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+	public static int add(int a,int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		int sum=add(10,20);
+		System.out.println("Sum of 10 and 20: "+sum);
+	}
+}
